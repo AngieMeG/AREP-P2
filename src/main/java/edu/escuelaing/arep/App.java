e3bd8988c1916f32834e75089c2ba0e75e115ea9
AngieMeG <70661112+AngieMeG@users.noreply.github.com>
2021-10-13 14:22:40 -0500

    Aplicacion local terminada

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,13 +1,60 @@
 package edu.escuelaing.arep;
 
+import static spark.Spark.*;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+import edu.escuelaing.arep.services.Matematica;
+import spark.Request;
+import spark.Response;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        port(getPort());
+        staticFileLocation("/public");
+
+        get("/", (req, res) -> {
+            res.redirect("/login.html");
+            res.status(200);
+            return null;
+        });
+
+        get("/atan", (req, res) -> atanFunction(req, res));
+        
+        get("/In", (req, res) -> inFunction(req, res));
+
+    }
+
+
+    public static JSONObject atanFunction(Request req, Response res){
+        res.type("application/json");
+        JSONObject response = new JSONObject();
+        response.put("operation", "atan");
+        response.put("input", req.queryParams("value"));
+        response.put("output", String.valueOf(Matematica.arcoTan(Double.parseDouble(req.queryParams("value")))));
+        return response;
+    }
+
+    public static JSONObject inFunction(Request req, Response res){
+        res.type("application/json");
+        JSONObject response = new JSONObject();
+        response.put("operation", "atan");
+        response.put("input", req.queryParams("value"));
+        response.put("output", String.valueOf(Matematica.logNatural(Double.parseDouble(req.queryParams("value")))));
+        return response;
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
---
+++ src/main/java/edu/escuelaing/arep/services/Matematica.java
@@ -0,0 +1,11 @@
+package edu.escuelaing.arep.services;
+
+public class Matematica {
+    public static Double arcoTan(Double number){
+        return Math.atan(number);
+    }
+
+    public static Double logNatural(Double number){
+        return Math.log(number);
+    }
+}
